daf0c4a11f0b3ac91f5b8f0101ac68f297d78de7
gaiusYan <gaiusyanbena11@gmail.com>
2024-12-17 00:50:29 +0100

    application security config

--- src/main/java/com/student/demo/appuser/AppUserRepository.java
+++ src/main/java/com/student/demo/appuser/AppUserRepository.java
@@ -2,9 +2,11 @@ package com.student.demo.appuser;
 
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.transaction.annotation.Transactional;
 
 @Transactional(readOnly = true)
-public interface AppUserRepository {
+public interface AppUserRepository 
+    extends JpaRepository<AppUser,Long>{
     Optional<AppUser> findByEmail(String email);
 }
